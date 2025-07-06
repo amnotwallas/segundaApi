4635729bc41033b22770111c02134c30ec373c86
amnotwallas <walterardev@gmail.com>
2025-07-05 20:53:42 -0600

    refactor(service): inject UserRepository via constructor in UserService

--- segundaApi/src/main/java/com/notwallas/segundaApi/service/UserService.java
+++ segundaApi/src/main/java/com/notwallas/segundaApi/service/UserService.java
@@ -4,31 +4,33 @@ import com.notwallas.segundaApi.repository.UserRepository;
 
 
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public User CreateUser(String nombre, int edad, String email, String password, String carrera) {
         User user = new User(nombre, edad, email, password, carrera);
-        UserRepository userRepository = new UserRepository();
         userRepository.saveUser(user);
         return user;
     }
 
     public String GetAllUsers() {
         String allUsers = "";
-        UserRepository userRepository = new UserRepository();
         for (User user : userRepository.findAllUsers()) {
-            allUsers += "ID: " + user.getId() + ", Name: " + user.getName() + 
-                        ", Age: " + user.getAge() + ", Email: " + user.getEmail() + 
+            allUsers += "ID: " + user.getId() + ", Name: " + user.getName() +
+                        ", Age: " + user.getAge() + ", Email: " + user.getEmail() +
                         ", Carrera: " + user.getCarrer() + "\n";
         }
-        return allUsers.toString();
+        return allUsers;
     }
 
     public User GetUserById(int id) {
-        UserRepository userRepository = new UserRepository();
         return userRepository.findUserById(id);
     }
 
     public boolean UpdateUser(int id, String name, int age, String email, String password, String carrer) {
-        UserRepository userRepository = new UserRepository();
         User user = userRepository.findUserById(id);
         if (user != null) {
             user.setName(name);
@@ -42,9 +44,7 @@ public class UserService {
         }
     }
 
-
     public void DeleteUser(int id) {
-        UserRepository userRepository = new UserRepository();
         userRepository.deleteUser(id);
         System.out.println("User deleted with ID: " + id);
     }
