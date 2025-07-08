49ace72205ec378ea9df4e2884c0fe1adecd21ba
amnotwallas <walterardev@gmail.com>
2025-07-08 15:28:25 -0600

    fix(repository): annotate UserRepository with @Repository to enable Spring injection

--- segundaApi/src/main/java/com/notwallas/segundaApi/repository/UserRepository.java
+++ segundaApi/src/main/java/com/notwallas/segundaApi/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.notwallas.segundaApi.repository;
-
+import com.notwallas.segundaApi.entity.User;
 import java.util.ArrayList;
 import java.util.List;
-import com.notwallas.segundaApi.entity.User;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepository {
     // Lista privada para almacenar los usuarios en memoria
     private static List<User> users = new ArrayList<>();
